b95f07aa603f6bd1268bb523be1e64b1baf9d8a4
ryanoliveir <ryandehallo229@gmail.com>
2023-11-05 23:30:02 -0300

    add: register page
    add: bootstrap file (styles)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 
 class Game():
@@ -8,18 +8,37 @@ class Game():
         self.console = console
 
 
+game1 = Game('Tetris', 'Puzzle', 'Atari')
+game2 = Game('God of War', 'Rack n Slash', 'PS2')
+game3 = Game('CSGO', 'FPS', 'PC')
+game4 = Game('Mortal Combat', 'Fight', 'PS2')
+
+games = [game1, game2, game3, game4]
+
+
 app = Flask(__name__)
 
 
-@app.route('/home')
-def home():
+@app.route('/register', methods=['POST'])
+def register():
+    name = request.form['name']
+    category = request.form['category']
+    console = request.form['console']
 
-    game1 = Game('Tetris', 'Puzzle', 'Atari')
-    game2 = Game('God of War', 'Rack n Slash', 'PS2')
-    game3 = Game('CSGO', 'FPS', 'PC')
-    game4 = Game('Mortal Combat', 'Fight', 'PS2')
+    print(name, category, console)
+    new_game = Game(name, category, console)
+    games.append(new_game)
+    
 
-    games = [game1, game2, game3, game4]
+    # return render_template('list.html', titles='Games', games=games)
+    return redirect('/')
+
+@app.route('/register-game')
+def register_game():
+    return render_template('game-form.html', title='Novo Jogo')
+
+@app.route('/')
+def index():
     return render_template('list.html', title='Games', games=games)
 
 
