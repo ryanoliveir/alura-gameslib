fc17fedb70baea25e50f1a916254e71ffa61accd
ryanoliveir <ryandehallo229@gmail.com>
2023-11-05 22:18:06 -0300

    add: dynamic content in html page

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask((__name__))
+
+class Game():
+    def __init__(self, name, category, console):
+        self.name = name
+        self.category = category
+        self.console = console
+
+
+app = Flask(__name__)
 
 
 @app.route('/home')
 def home():
-    return '<h1>Home Page</h1>'
 
-app.run()
+    game1 = Game('Tetris', 'Puzzle', 'Atari')
+    game2 = Game('God of War', 'Rack n Slash', 'PS2')
+    game3 = Game('CSGO', 'FPS', 'PC')
+    game4 = Game('Mortal Combat', 'Fight', 'PS2')
+
+    games = [game1, game2, game3, game4]
+    return render_template('list.html', title='Games', games=games)
+
+
+if(__name__ == '__main__'):
+    app.run(debug=True)
