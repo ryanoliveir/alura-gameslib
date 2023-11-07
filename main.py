6412047828f5e660ad3bd2de6477772b9b5d737f
ryanoliveir <ryandehallo229@gmail.com>
2023-11-06 21:39:00 -0300

    Login and Auth
    add: login page
    
    add: auth endpoint (/authentication)
    
    add: password hash

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask, render_template, request, redirect, session
+from flask_bcrypt import Bcrypt  
 
 class Game():
     def __init__(self, name, category, console):
         self.name = name
         self.category = category
         self.console = console
-
 
 game1 = Game('Tetris', 'Puzzle', 'Atari')
 game2 = Game('God of War', 'Rack n Slash', 'PS2')
@@ -17,7 +16,28 @@ games = [game1, game2, game3, game4]
 
 
 app = Flask(__name__)
+app.secret_key = 'flaks_secret_key'
+bcrypt = Bcrypt(app)
 
+USER = 'ryan'
+PASSWORD = '123456'
+PASSWORD_HASH = bcrypt.generate_password_hash(PASSWORD)
+
+
+@app.route('/authentication', methods=['POST'])
+def authentication():
+    user = request.form['user']
+    password = request.form['password']
+
+    if user == USER and bcrypt.check_password_hash(PASSWORD_HASH, password):
+        session['user'] = user
+        return redirect('/')
+
+    return redirect('/login')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -39,7 +59,11 @@ def register_game():
 
 @app.route('/')
 def index():
-    return render_template('list.html', title='Games', games=games)
+    if 'user' in session:
+        print(session)
+        return render_template('list.html', title='Games', games=games)
+    
+    return redirect('/login')
 
 
 if(__name__ == '__main__'):
